f0c7ae12eda3d3cfa092177bc765746935765374
SLMakomazi <lutho.makomazi@gmail.com>
2025-04-08 16:06:52 +0200

    Added the swagger documentation code to  the order controller & pom.xml files

--- src/main/java/com/order_service/Controller/OrderController.java
+++ src/main/java/com/order_service/Controller/OrderController.java
@@ -2,19 +2,23 @@ package com.order_service.Controller;
 
 import com.order_service.Model.Order;
 import com.order_service.Service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/orders")
+@Tag(name = "Order API", description = "API for managing orders")
 public class OrderController {
 
     @Autowired
     private OrderService orderService;
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get an order by ID", description = "Retrieve an order by its unique ID")
     public Order getOrder(@PathVariable Long id) {
         return orderService.getOrder(id);
     }
@@ -25,6 +29,7 @@ public class OrderController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new order", description = "Add a new order to the system")
     public Order createOrder(@RequestBody Order order) {
         return orderService.createOrder(order);
     }
